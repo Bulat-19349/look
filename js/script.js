f2f09fb55974958904816b55f7a394a48785499f
Bulat <bulich-19349@mail.ru>
2020-11-29 17:25:52 +0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+
+// // Filter
+
+// let name = ["Ivan", "Alex", "Volendemort"];
+// let shortName = name.filter(function(name){
+//     return name.length < 5;
+// });
+// console.log(item);
+
+
+// Map
+
+
+// let answers = ['IvAn', 'AnnA', 'KSenIya'];
+
+// let result = answers.map(item =>{
+//     return item.toLowerCase();
+// });
+// console.log(result);
+
+// let some = [4, 'wew', 'qww'];
+// console.log(some.some(item => {
+//     return typeof(item) === 'number';
+// }))
+
+// Reduce
+
+// let arr = ['Apple', 'Plum', 'Tomat'];
+
+// let res = arr.reduce((sum, i) => {
+//     return `${sum}, ${i}`;
+// })
+// console.log(res);
+
+// let obj = {
+//     Ivan: 'persone',
+//     Anna: 'persone',
+//     dog: 'animal'
+// }
+// let newArr = Object.entries(obj)
+// .filter(item => {
+//     return item[1] === 'persone';
+// }).map(item =>{
+//     return item[0];
+// })
+// console.log(newArr);
+localStorage.setItem('number', 5);
+console.log(localStorage.getItem('number'));
